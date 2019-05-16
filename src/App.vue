3e0f8561bed6f78940a07474e010a4b0ca263281
simon ndungu <mac@skndungu-2.local>
2019-05-16 10:08:30 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  name: "App",
+  components: {
+    Navbar
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/Index'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '/',
+    name: 'Index',
+    component: Index
+  }]
+})
